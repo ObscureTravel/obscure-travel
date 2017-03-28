a393399ffffc52481ef1281265eae081601b32e1
terrythompson <b.terry.thompson@gmail.com>
2017-03-28 15:15:55 -0400

    created Location controller and repo

---
+++ src/main/java/obscureTravel/LocationRepository.java
@@ -0,0 +1,12 @@
+package obscureTravel;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface LocationRepository extends CrudRepository<Location, Long> {
+
+	List<Location> findAll();
+	List<Location> findByName(String name);
+	Location findOne(Long id);
+}
