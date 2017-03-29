190d507c619511179922083539f7fc40a79cf1e9
terrythompson <b.terry.thompson@gmail.com>
2017-03-29 15:38:05 -0400

    recovered index and import.sql

--- src/main/java/obscureTravel/LocationRepository.java
+++ src/main/java/obscureTravel/LocationRepository.java
@@ -1,12 +1,7 @@
 package obscureTravel;
 
-import java.util.List;
-
 import org.springframework.data.repository.CrudRepository;
 
 public interface LocationRepository extends CrudRepository<Location, Long> {
 
-//	List<Location> findAll();
-//	List<Location> findByName(String name);
-//	Location findOne(Long id);
 }
