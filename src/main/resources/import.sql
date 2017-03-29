df78d646978e8c2bb7019708e31103f3fd9c5530
terrythompson <b.terry.thompson@gmail.com>
2017-03-29 11:49:53 -0400

    fixed Gradle dependencies

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,1 @@
-insert into Location(String name, String latitude, String longitude, String locationType, String description) value Location("Bossy Grrl's PinUp Joint", "40.0153", "-83.0114", "Bar", "description")
+insert into Location(name, latitude, longitude, location_Type, description) values ('Bossy Grrl''s PinUp Joint', '40.0153', '-83.0114', 'Bar', 'description')
