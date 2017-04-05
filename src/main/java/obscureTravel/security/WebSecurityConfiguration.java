873550907525fc9a8303435977800d9d190f825e
B T Forsythe <brian@btforsythe.io>
2017-04-05 14:20:46 -0400

    disabled frame options to allow console

--- src/main/java/obscureTravel/security/WebSecurityConfiguration.java
+++ src/main/java/obscureTravel/security/WebSecurityConfiguration.java
@@ -9,14 +9,15 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http //
-				.csrf().disable() //
-				.antMatcher("/**").authorizeRequests() //
+		http.csrf().disable();
+
+		http.antMatcher("/**").authorizeRequests() //
 				.antMatchers("/h2-console/**").permitAll() //
 				.anyRequest().authenticated();
+
+		http.headers().frameOptions().disable();
 	}
 
 }
